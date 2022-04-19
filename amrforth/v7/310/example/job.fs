98d0750592093e984770534a1f0fc5fdd70879bf
wa1tnr <wa1tnr@users.noreply.github.com>
2022-04-19 10:44:25 +0000

    UNSAFE GPIO - removed all active code
      thepins.fs and safepins.fs not good.
    
      Sets up push-pull and clears bits - not good.
    
    	modified:   job.fs
    
    On branch dvlp-cc

--- amrforth/v7/310/example/job.fs
+++ amrforth/v7/310/example/job.fs
@@ -1,2 +1,11 @@
 \ job.fs
 
+0 [if] \ a zero here will disable this code block
+  include ustimer-cwh.fs
+  \ include safepins.fs
+  \ include thepins.fs
+  \ include main.fs
+[then]
+
+\ : go 1 drop -;
+
