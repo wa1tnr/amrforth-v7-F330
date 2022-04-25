1c936209005ccf32165b2c315d8e8a91afec4f23
wa1tnr <wa1tnr@users.noreply.github.com>
2022-04-25 22:14:06 +0000

    possibly safer init
    	modified:   job.fs
    
    On branch dvlp-aa

--- amrforth/v7/310/example/job.fs
+++ amrforth/v7/310/example/job.fs
@@ -1,5 +1,18 @@
 \ job.fs
 
+include ustimer-cwh.fs
+
+code starting
+    $40 invert # PCA0MD anl   \ p. 216  WDTE watchdog disabled
+    $ff # P0MDIN orl          \ p. 136  No analog, all digital, port 0.x
+    $04 # P0MDOUT orl         \ p. 137  push-pull  for pin 2
+
+next c;
+
+code !2p0 2 .P0 cpl  next c;
+code _2p0 2 .P0 clr  next c;
+code  2p0 2 .P0 setb next c;
+
 0 [if] \ a zero here will disable this code block
   include ustimer-cwh.fs
   \ include safepins.fs
@@ -7,5 +20,19 @@
   \ include main.fs
 [then]
 
-\ : go 1 drop -;
 
+: blink ( -- )
+  !2p0 2000 ms !2p0 2000 ms
+;
+
+: blinks ( n -- )
+  for blink next
+;
+
+: run
+  starting
+  2p0 \ turns off LED by setting this pin HIGH
+  3 blinks
+;
+
+\ : go 1 drop -;
