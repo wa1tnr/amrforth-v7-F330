563c191a01a9ce5a29ff80c8145f22e3e8e00b41
wa1tnr <wa1tnr@users.noreply.github.com>
2024-03-05 23:58:11 +0000

    to sync with current rom files
    	modified:   job.fs
    
    On branch local-aa

--- amrforth/v7/330/test-march-2024.d/job.fs
+++ amrforth/v7/330/test-march-2024.d/job.fs
@@ -1,7 +1,7 @@
 \ job.fs
 include ustimer-cwh.fs
 include main.fs
-: mine (  - ) 55 drop ;
+: mine (  - ) cr cr ." nearo 23:19z hello you all" cr cr 55 drop ;
 0 [if] \ a zero here will disable this code block
   include ustimer-cwh.fs
   include main.fs
