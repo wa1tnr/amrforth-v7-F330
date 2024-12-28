f435f10ef884ddf8032e0b1ddd83cf277449d5aa
wa1tnr <wa1tnr@users.noreply.github.com>
2024-12-28 23:35:32 +0000

    UNTESTED turnkey app - simple exerciser
      blinks and sends USART msgs 9600
    
      P0.2 is believed to be blinking but not
      directly inspected.  P2.0 does not seem
      to play a role - enable disable words
      w shorter spellings are ignored.
    
    	modified:   main.fs
    
    On branch dvlp-aa

--- amrforth/v7/330/test-dec-2024-ab.d/main.fs
+++ amrforth/v7/330/test-dec-2024-ab.d/main.fs
@@ -1,17 +1,38 @@
 \ main.fs -- f330 example program
 \ Fri 27 Dec 21:59:56 UTC 2024
 
+: xdatex
+  ." Sat 28 Dec 23:25:02 UTC 2024 "
+;
+
+code enbl (  - ) 0 .P2 clr  next c;
+code dsbl (  - ) 0 .P2 setb next c;
+
+\ : iterVal 14 1 + ; \ sum is 15
+
+\ Since 8 blinks are reliable, timed against serial prints,
+\ and since that is the intent, makes better sense to
+\ specify iterVal as ( iterations 2 * ) - 1, where - 1 was
+\ determined experimentally.
+\ 8 2 * 1 -
+
+\ : iterVal 16 1 - ; \ sum is 15
+: iterVal 8 2 * 1 - ; \ outcome is 15
+
+: bounds0< -1 max iterVal 1 - min ;
+
 code startup
 	$40 invert # PCA0MD anl  \ Clear watchdog enable bit.
 	$ff # P0MDIN orl   \ No analog, all digital, port 0.x
         $cf # P0MDOUT orl  \ all pins output, push pull -- except 4 and 5
+        $01 # P2MDOUT orl  \ push pull
+
 	$01 # XBR0 mov  \ Enable TX and RX on P0.4, P0.5.
 	$40 # XBR1 mov  \ Enable crossbar and weak pull-ups.
 \       2 .P0 clr \ initialize the LED's GPIO pin so that the LED is not lit.
 	next c;
-\ short
 
-code ncpl 2 .P0 cpl next c;
+code ncpl 2 .P0 cpl next c; \ colon element may be here! lotta bangs today!
 
 \ works without type - why:
 : rjcpl ." test alias 23:24z Fri 27th" cr cr .s cr cr space space ncpl ; \ was: type space
@@ -23,6 +44,7 @@ code ncpl 2 .P0 cpl next c;
 ;
 
 variable counted
+variable iterations
 
 : gonoa
   startup
@@ -77,5 +99,35 @@ variable counted
   begin
   1 drop
   again
+;
+
+: message space ." ln: this is a message" space ;
+
+: setup 1 iterations ! ;
+
+: triggered space ." TRIGGERED " space ;
+
+: resetIter iterVal iterations ! ;
+
+\ all go-like words ignored above.  may not be
+\ useful in present context.  Unexplored.
+
+: go -99 dup 1 + dup 1 +
+  3000 ms \ time to logon after power cycle
+  cr xdatex cr cr
+  ." go is running.. "
+  resetIter
+  ." greely colorado " cr
+  begin
+    iterations @ 1 - iterations ! iterations @
+    bounds0<
+    0< IF
+      message
+      resetIter
+    THEN
+    ncpl
+    500 ms
+  again
 -;
+
 \ end.
