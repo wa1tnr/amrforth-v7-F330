726087759af7324be0e1a4a053b83e5d9c27d4b9
wa1tnr <wa1tnr@users.noreply.github.com>
2025-01-08 22:37:05 +0000

    substitutions - examine carefully
      Some committed here point to correct current resource.
    
      Follow those and complete as required.
    
      The dothis.sh script may model this correctly - untested.
    
    	deleted:    c2
    	new file:   c2---
    	deleted:    c2.fs
    	new file:   c2.fs---
    	new file:   dothis.sh
    	deleted:    gforth
    	new file:   gforth---
    	deleted:    gforth.fi
    	new file:   gforth.fi---
    	deleted:    jtag_c2.fs
    	new file:   jtag_c2.fs---
    
    On branch dvlp-aa

---
+++ amrforth/v7/330/linked/dothis.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cp -p \
+  c2--- \
+  c2
+
+cp -p \
+  c2.fs--- \
+  c2.fs
+
+cp -p \
+  gforth--- \
+  gforth
+
+cp -p \
+  gforth.fi--- \
+  gforth.fi
+
+cp -p \
+  jtag_c2.fs--- \
+  jtag_c2.fs
