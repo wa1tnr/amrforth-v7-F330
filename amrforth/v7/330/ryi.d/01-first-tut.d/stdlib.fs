1560204a78fed953e3f994b4ed592df4c4af1d15
wa1tnr <wa1tnr@users.noreply.github.com>
2025-01-09 23:54:14 +0000

    Tentatively complete - may compile from clone
      UNTESTED for completeness
    
    	renamed:    gpio-init-a.fs -> gpio-init-abb.fs
    	modified:   stdlib.fs
    
    On branch dvlp-aa

--- amrforth/v7/330/ryi.d/01-first-tut.d/stdlib.fs
+++ amrforth/v7/330/ryi.d/01-first-tut.d/stdlib.fs
@@ -1,14 +1,13 @@
 \ stdlib.fs
-\ Thu  9 Jan 23:39:24 UTC 2025
+\ Thu  9 Jan 23:53:54 UTC 2025
 
 cr
 5 spaces .( loading stdlib.fs) cr
 
 include ustimer-cwh.fs
-\ include gpio-lib-a.fs
-include gpio-lib-abb.fs \ not checked if 'standard' version or modified lately
-\ include gpio-init-a.fs
-include gpio-init-abb.fs \ again was not checked against a 'standard' version
+include gpio-lib-a.fs \ substantially the same as earlier version
+\ include gpio-init-a.fs \ rescinded a more complete version
+include gpio-init-abb.fs \ NOT the standard version - pared down some
 
 : stkpad -99 dup 1 + dup 1 + ;
 : 2r 2^raised ;
