dc805b0f6142b1f326ab5c3d4520bd9bdee35974
wa1tnr <wa1tnr@users.noreply.github.com>
2025-01-09 23:50:39 +0000

    LARGE edit - pay attention
      deleted unused stuff for THIS application only.
    
      will rename as well
    
    	modified:   gpio-init-a.fs
    
      future name will be  gpio-init-abb.fs
    
    On branch dvlp-aa

--- amrforth/v7/330/ryi.d/01-first-tut.d/gpio-init-a.fs
+++ amrforth/v7/330/ryi.d/01-first-tut.d/gpio-init-a.fs
@@ -1,21 +1,7 @@
-\ Tue 31 Dec 20:54:14 UTC 2024
+\ gpio-init-a.fs
+\ Thu  9 Jan 17:17:01 UTC 2025
 
-: allrlow ( - )
-  7 .P1 clr
-  6 .P1 clr
-  5 .P1 clr
-  4 .P1 clr
-  3 .P1 clr
-  \ 2 .P1 clr
-  1 .P1 clr
-  0 .P1 clr
-  7 .P0 clr
-  6 .P0 clr
-  3 .P0 clr
-  2 .P0 clr
-  1 .P0 clr
-  0 .P0 clr
-;
+5 spaces .( loading gpio-init-a.fs) cr
 
 code initPCA
         $40 invert # PCA0MD anl  \ Clear watchdog enable bit.
@@ -23,16 +9,15 @@ code initPCA
 	next c;
 
 code initPortIO
-        $cf # P0MDOUT orl  \ all pins output, push pull -- except 4 and 5
-	$ff # P0MDIN orl   \ No analog, all digital, port 0.x
+        \ $cf # P0MDOUT orl  \ all pins output, push pull -- except 4 and 5
+	\ $ff # P0MDIN orl   \ No analog, all digital, port 0.x
 
-        $ff # P1MDOUT orl
-	$ff # P1MDIN  orl  \ sketchy
-
-	$01 # XBR0 mov  \ Enable TX and RX on P0.4, P0.5.
+        $07 # P1MDOUT mov
+	$07 # P1MDIN orl   \ digital, not analog, P1.1, P1.0
+        $13 # P1SKIP mov
 	$40 # XBR1 mov  \ Enable crossbar and weak pull-ups.
 	next c;
 
-: init initPCA initPortiO allrlow ;
+: init initPCA initPortiO ;
 
 \ end.
