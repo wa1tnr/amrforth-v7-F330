f9080a7764f90806b54cfaf641a7f773398eea2e
wa1tnr <wa1tnr@users.noreply.github.com>
2025-01-13 16:53:44 +0000

    minor maintenance commit
    	deleted:    ../../../../0-Distrib/.gitignore
    	modified:   job.fs
    
    On branch dvlp-aa

--- amrforth/v7/330/morse-2025/job.fs
+++ amrforth/v7/330/morse-2025/job.fs
@@ -1,7 +1,8 @@
 \ job.fs
-include ustimer-cwh.fs
-include main.fs
-: mine (  - ) cr cr ." nearo 23:19z hello you all" cr cr 55 drop ;
+\ include ustimer-cwh.fs
+\ include main.fs
+include morse.fs
+: mine (  - ) cr cr ." nearo 13 Jan 2025 14:31z hello you all" cr cr 55 drop ;
 0 [if] \ a zero here will disable this code block
   include ustimer-cwh.fs
   include main.fs
